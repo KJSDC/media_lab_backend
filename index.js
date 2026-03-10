415d2a009bd5e0d0cfb8272bf34f435748ec0d6d
Nrj007 <21bcac44@kristujayanti.com>
2026-03-10 12:43:16 +0530

    movement changes 2

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const { Pool } = require("pg");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 // Middleware
 app.use(cors());
 app.use(express.json());
